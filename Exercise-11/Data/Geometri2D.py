b5abbd96b57ee38738b43b157248e862caefe702
Izzananda <izzanadimasfaza15@student.uns.ac.id>
2022-05-07 19:49:09 +0700

    Mengerjakan Exercise 11

---
+++ Exercise-11/Data/Geometri2D.py
@@ -0,0 +1,25 @@
+import math
+
+def LuasPersegiPanjang(p,l):
+    return p*l
+
+def KelilingPersegiPanjang(p,l):
+    return 2*(p+l)
+
+def LuasBujurSangkar(s):
+    return s*s
+
+def KelilingBujurSangkar(s):
+    return 4*s
+
+def LuasLingkaran(r):
+    return math.pi*r*r
+
+def KelilingLingkaran(r):
+    return 2*math.pi*r
+
+def LuasSegitiga(a,t):
+    return (a*t)/2
+
+def KelilingSegitiga(a,b,c):
+    return a+b+c
---
+++ Exercise-11/README.md
@@ -0,0 +1,2 @@
+# Exercise-11
+Repositori ini berisi materi dan tugas minggu ke-11. Silahkan dipelajari dan dikerjakan kemudian diupload ke repositori kalian masing-masing. 
